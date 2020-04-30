06eb1e1f2e6462448babb1080ce938fa03cd96d7
tusk2019 <53996136+tusk2019@users.noreply.github.com>
2020-04-30 12:42:42 +0900

    first commit

---
+++ js/jquery.inview_set.js
@@ -0,0 +1,11 @@
+	$('.up').on('inview', function() {
+		$(this).addClass('upstyle');
+	});
+	
+	$('.left').on('inview', function() {
+		$(this).addClass('leftstyle');
+	});
+	
+	$('.right').on('inview', function() {
+		$(this).addClass('rightstyle');
+	});
